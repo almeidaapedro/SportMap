d3c280ad02b85a1842c0c6c7532e2c51c0905693
Pedro Almeida <almeida.pedroaugusto25@gmail.com>
2026-08-01 16:50:23 -0300

    ajustando a aplicação para ficar responsiva

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,30 +1,66 @@
+import { useState } from "react";
 import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  function closeMenu() {
+    setMenuOpen(false);
+  }
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
 
         <div className={styles.logo}>
-          <Link to="/">
+          <Link to="/" onClick={closeMenu}>
             Sport Map
           </Link>
         </div>
 
-        <nav className={styles.menu}>
-          <Link to="/">Início</Link>
+        <nav className={`${styles.menu} ${menuOpen ? styles.active : ""}`}>
+          <Link to="/" onClick={closeMenu}>
+            Início
+          </Link>
 
-          <Link to="/about">Sobre</Link>
+          <Link to="/about" onClick={closeMenu}>
+            Sobre
+          </Link>
 
-          <Link to="/explore">Explorar</Link>
+          <Link to="/explore" onClick={closeMenu}>
+            Explorar
+          </Link>
 
-          <Link to="/contact">Contato</Link>
+          <Link to="/contact" onClick={closeMenu}>
+            Contato
+          </Link>
+
+          <Link 
+            to="/explore" 
+            className={styles.mobileButton}
+            onClick={closeMenu}
+          >
+            Buscar quadras
+          </Link>
         </nav>
+
 
         <Link to="/explore" className={styles.button}>
           Buscar quadras
         </Link>
+
+
+        <button
+          className={styles.hamburger}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
 
       </div>
     </header>
